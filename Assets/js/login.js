bb8114a2bed1ab9dd80402d1aa14f655440f45a7
Hyosuporte <99928498+Hyosuporte@users.noreply.github.com>
2022-09-30 19:36:13 -0500

    Actualizacion "login.js"
    Se agrego la funcion correspondiente con la validacion de la informacion del usuario y los campos null

---
+++ Assets/js/login.js
@@ -0,0 +1,28 @@
+function frmLogin(e) {
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  if (email.value == "") {
+    //TODO: Agregar css para indicar que el usuario es obligatorio
+  } else if (password.value == "") {
+    //TODO: Agregar css para indicar que la contraseña es obligatoria
+  } else {
+    const url = base_url + "usuario/validar";
+    const frm = document.getElementById("frmLogin");
+    const http = new XMLHttpRequest();
+    http.open("POST", url, true);
+    http.send(new FormData(frm));
+    http.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        const res = JSON.parse(this.responseText);
+        console.log(res);
+        if (res == "1") {
+          window.location = base_url + "administrador";
+        } else if (res == "2") {
+          window.location = base_url + "cliente";
+        } else {
+          //TODO: Agregar css para indicar al usuario porque fallo la validacion
+        }
+      }
+    };
+  }
+}
