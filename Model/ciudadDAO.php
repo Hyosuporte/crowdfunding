27883152615dbc96487d082afb39f6fb119df8ce
Daniel Torres <dmtm1404@gmail.com>
2022-10-03 13:47:34 -0500

    Formulario registro y proyectos
    @Hyosuporte  aqui ya esta funcionando el selector de ciudad
    @jjleon22  aqui arregle el orden del formulario proyectos

---
+++ Model/ciudadDAO.php
@@ -0,0 +1,16 @@
+<?php
+    class ciudadDAO extends Query
+    {
+        public function __construct()
+        {
+            parent::__construct();
+        }    
+
+        public function getCiudades(int $id_pais)
+        {
+            $sql = "SELECT * FROM ciudad WHERE id_pais=$id_pais";
+            $data = $this->selectAll($sql);
+            return $data;
+        }
+    }
+?>
