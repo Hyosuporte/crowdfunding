cb828fed03f10beb6d5bc19f8ecbc7d9ca7d84dc
Daniel Torres <dmtm1404@gmail.com>
2022-11-15 22:44:52 -0500

    Galeria Proyectos

--- View/Template/header.php
+++ View/Template/header.php
@@ -18,13 +18,13 @@
 <nav class="navbar fixed-top">
         <ul class="navbarList">
             <li>
-                <span class="navbar-brand navbarLogo"><img src="<?php echo BASE_URL; ?>Assets/img/LOGO_21.svg" alt=""></span>
+                <a href="<?php echo BASE_URL; ?>Home/index"><span class="navbar-brand navbarLogo"><img src="<?php echo BASE_URL; ?>Assets/img/LOGO_21.svg" alt=""></span></a>
             </li>
             <li>
-                <a href="">Ver Proyectos</a>
+                <a href="">Descubre</a>
             </li>
             <li>
-                <a href="">Subir Proyecto</a>
+                <a href="">Aplica</a>
             </li>
             <li>
                 <div><a href="<?php echo BASE_URL; ?>Home/login/"><button class="botonLogin" type="button">Llamado acción</button></a></div>
