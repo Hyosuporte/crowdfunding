014d060332682766ca15ca3d4c59dfc2c9d5cfc6
jjleon22 <114668871+jjleon22@users.noreply.github.com>
2022-11-19 23:09:19 -0500

    Capturadatos

--- mercado_pago.php
+++ mercado_pago.php
@@ -9,6 +9,13 @@ $item->title = 'Titulo del Item';
 $item->quantity = 1;
 $item->unit_price = 75; //traer del array de donación
 $preference->items = array($item);
+$preference->urls = array(
+    "success" => "http://localhost/crowfunding/infotr.php", //pago exitoso
+    "failure" => "http://localhost/crowdfunding/mercado_pago.php" //fallo en el pago puede redirecciónar a otra parte
+);
+$preference->autoreturn = "approved";
+$preference->binary_mode = "false"; //crear script de escucha para pagos pendientes
+
 $preference->save();
 ?>
 
