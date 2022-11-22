7e1c0e9f44e3f42917e96f243795afb3482fbf03
Daniel Torres <dmtm1404@gmail.com>
2022-11-22 13:52:02 -0500

    Pequeño arreglo en los inputs del registro

--- View/registro.php
+++ View/registro.php
@@ -11,16 +11,16 @@
         </div>
         <form action="" method="post" id="frmRegistro">
             <div class="inputsRegistro">
-                <input type="text" class="form-control" placeholder="Nombre Completo" id="nombre" name="nombre" required max="20">
-                <input type="email" class="form-control" placeholder="Email" id="correo" name="correo" required max="40">
+                <input type="text" class="form-control inputFormularioRegistrar" placeholder="Nombre Completo" id="nombre" name="nombre" required max="20">
+                <input type="email" class="form-control inputFormularioRegistrar" placeholder="Email" id="correo" name="correo" required max="40">
                 <div style="display: flex;">
-                    <input type="password" class="form-control" placeholder="Contraseña" id="password" name="password" required max="15">
-                    <input type="password" class="form-control" placeholder="Confirmar Contraseña" id="passwordConf" name="passwordConf" required max="15">
+                    <input type="password" class="form-control inputFormularioRegistrarGrupo" placeholder="Contraseña" id="password" name="password" required max="15">
+                    <input type="password" class="form-control inputFormularioRegistrarGrupo" placeholder="Confirmar Contraseña" id="passwordConf" name="passwordConf" required max="15">
                 </div>
                 <input type="checkbox" name="recordarme" id="terminos" name="terminos" class="formInputCheck">
                 <label for="terminos">Acepto terminos y condiciones de TFI</label>
             </div>
-            <button type="button" class="botonCardDestacado mt-5" onclick="frmRegistro()">Llamado acción</button>
+            <button type="button" class="botonCardDestacado mt-5" onclick="frmRegistro()">Enviar</button>
             <div class="linkRegistrarse">
                 <p>¿Ya tienes una cuenta? <a href="<?php echo BASE_URL; ?>Home/login/">INICIA SESIÓN</a></p>
             </div>
