4dfe6c648568489b0bfb4027ff14f1602584a8fb
jjleon22 <114668871+jjleon22@users.noreply.github.com>
2022-11-22 21:48:01 -0500

    Update mercado_pago.php

--- mercado_pago.php
+++ mercado_pago.php
@@ -2,19 +2,19 @@
 // SDK de Mercado Pago
 require __DIR__ .  '/vendor/autoload.php';
 // Agrega credenciales
-MercadoPago\SDK::setAccessToken(''); //key private access
+MercadoPago\SDK::setAccessToken('TEST-5266226948659448-110816-6aea6b9808bcb46adc9c2237700cb0f6-1217455549'); //key private access
 $preference = new MercadoPago\Preference();
 $item = new MercadoPago\Item();
 $item->title = 'Titulo del Item';
 $item->quantity = 1;
 $item->unit_price = 75; //traer del array de donación
 $preference->items = array($item);
-$preference->urls = array(
+/*$preference->urls = array(
     "success" => "http://localhost/crowfunding/infotr.php", //pago exitoso
     "failure" => "http://localhost/crowdfunding/mercado_pago.php" //fallo en el pago puede redirecciónar a otra parte
-);
-$preference->autoreturn = "approved";
-$preference->binary_mode = "false"; //crear script de escucha para pagos pendientes
+);*/
+//$preference->autoreturn = "approved"; //aqui estaba el error
+//$preference->binary_mode = "false"; //crear script de escucha para pagos pendientes
 
 $preference->save();
 ?>
@@ -26,11 +26,14 @@ $preference->save();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Integración Mercado Pago</title>
+    <meta http-equiv="Content-Security-Policy" content="">
     <script src="https://sdk.mercadopago.com/js/v2"></script>
+    <title>Integración Mercado Pago</title>
+    
 </head>
 
 <body>
+
     <h3>Esto es mercado pago</h3>
     <div class="checkout-btn"></div>
     <script>
@@ -48,6 +51,8 @@ $preference->save();
             }
         })
     </script>
+    
 </body>
 
 </html>
+
