d4bf5f9a2a92a61059273e0aa354e9b511abce32
jjleon22 <114668871+jjleon22@users.noreply.github.com>
2022-12-23 01:19:59 -0500

    Create Config.php

---
+++ Config/Config.php
@@ -0,0 +1,10 @@
+<?php
+const BASE_URL = "http://localhost/crowdfunding/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const BD = "bd_crowdfunding";
+const CHARSET = "charset=utf8";
+const TITLE = "CrowdFunding";
+const dir_subida = 'C:\\xampp\\htdocs\\crowdfunding\\uploadeddocuments\\';
+?>
