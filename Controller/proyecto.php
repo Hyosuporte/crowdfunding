2b50fd97664bee68cadb4fc872fe32fed5a9bf7d
jjleon22 <jjleons@correo.udistrital.edu.co>
2023-01-05 13:23:49 -0500

    Update proyecto.php

--- Controller/proyecto.php
+++ Controller/proyecto.php
@@ -45,19 +45,6 @@ class proyecto extends Controller
 
     public function insertarproyecto()
     {
-        $keywords = strClean($_POST['keywords']);
-        $tiempo_ejecucion = strClean($_POST['ejecucion']);
-        $titulo = strClean($_POST['titulo']);
-        $duracion_campana = strClean($_POST['campana']); 
-        $fecha_comienzo = strClean($_POST['comienzo']); 
-        $fecha_final = strClean($_POST['final']);
-        $abstrac = strClean($_POST['Abstract']);
-        $indicador = strClean($_POST['impacto']);
-        $monto = strClean($_POST['monto']);
-        $video = strClean($_POST['video']);
-        print_r($_POST['campana']);
-        $foto = $this->subirArchivo($_POST['foto']);
-        $info_adicional = strClean($_POST['info']);
         $camara = $this->subirArchivo($_POST['camara']);
         $RUT = $this->subirArchivo($_POST['RUT']);
         $rep_legal = $this->subirArchivo($_POST['rep_legal']);
@@ -65,6 +52,18 @@ class proyecto extends Controller
         $bancario = $this->subirArchivo($_POST['bancario']);
         $aprob_donacion = $this->subirArchivo($_POST['aprob_donacion']);
         $form_declaraciones = $this->subirArchivo($_POST['form_declaraciones']);
+        $keywords = strClean($_POST['keywords']);
+        $tiempo_ejecucion = strClean($_POST['ejecucion']);
+        $titulo = strClean($_POST['titulo']);
+        $foto = $_FILES[$_POST['foto']];
+        $duracion_campana = strClean($_POST['comienzo']); //pendiente actualizar
+        $fecha_comienzo = strClean($_POST['comienzo']); //pendiente actualizar
+        $fecha_final = strClean($_POST['final']);
+        $abstrac = strClean($_POST['Abstract']);
+        $indicador = strClean($_POST['impacto']);
+        $monto = strClean($_POST['monto']);
+        $video = strClean($_POST['video']);
+        $info_adicional = strClean($_POST['info']);
 
         if (
             empty($camara) || empty($RUT) || empty($rep_legal) || empty($cedula) || empty($bancario) || empty($aprob_donacion) || empty($form_declaraciones) || empty($keywords) || empty($tiempo_ejecucion) || empty($titulo) || empty($foto) ||
@@ -98,7 +97,7 @@ class proyecto extends Controller
         $tiempo_ejecucion = strClean($_POST['ejecucion']);
         $titulo = strClean($_POST['titulo']);
         $foto = $_FILES[$_POST['foto']];
-        $duracion_campaña = strClean($_POST['comienzo']); //pendiente actualizar
+        $duracion_campana = strClean($_POST['comienzo']); //pendiente actualizar
         $fecha_comienzo = strClean($_POST['comienzo']); //pendiente actualizar
         $fecha_final = strClean($_POST['final']);
         $abstrac = strClean($_POST['Abstract']);
@@ -107,14 +106,16 @@ class proyecto extends Controller
         $video = strClean($_POST['video']);
         $info_adicional = strClean($_POST['info']);
 
+        
+
         if (
             empty($camara) || empty($RUT) || empty($rep_legal) || empty($cedula) || empty($bancario) || empty($aprob_donacion) || empty($form_declaraciones) || empty($keywords) || empty($tiempo_ejecucion) || empty($titulo) || empty($foto) ||
-            empty($duracion_campaña) || empty($fecha_comienzo) || empty($fecha_final) || empty($abstrac) || empty($indicador)
+            empty($duracion_campana) || empty($fecha_comienzo) || empty($fecha_final) || empty($abstrac) || empty($indicador)
             || empty($monto) || empty($video) || empty($info_adicional)
         ) {
             $msg = "Todos los campos son obligatorios";
         } else {
-            $data = $this->model->editarproyecto($camara, $RUT, $rep_legal, $cedula, $bancario, $aprob_donacion, $form_declaraciones, $keywords, $tiempo_ejecucion, $titulo, $foto, $duracion_campaña, $fecha_comienzo, $fecha_final, $abstrac, $indicador, $monto, $video, $info_adicional);
+            $data = $this->model->editarproyecto($camara, $RUT, $rep_legal, $cedula, $bancario, $aprob_donacion, $form_declaraciones, $keywords, $tiempo_ejecucion, $titulo, $foto, $duracion_campana, $fecha_comienzo, $fecha_final, $abstrac, $indicador, $monto, $video, $info_adicional);
             if ($data === "ok") {
                 $msg = "proyecto editado";
             } else {
